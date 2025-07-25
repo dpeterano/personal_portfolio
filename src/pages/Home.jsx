67cd7ac30bdf340bd13e43e167779e2faeab5585
Dylan PETERANO <dylan.peterano.work@gmail.com>
2025-07-24 15:41:29 -1000

    Setup Config
    - Installed dependencies
    - Configurated the vite.config file with taliwindcss
    - Created 2 pages Home & NotFound Modules
    - Prepared index.css variables ( needs more work )

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return  <div className="min-h-screen">
+
+        {/* { Theme Toggle } */}
+
+        {/* { Background Effects } */}
+
+        { /* Navvbar */ }
+
+        {/* { Main Content } */}
+    </div>
+};
